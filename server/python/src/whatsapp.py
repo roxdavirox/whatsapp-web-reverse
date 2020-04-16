29751d8d27e93a45da64c181af3b6924d9d2948f
roxdavirox <roxdavirox@gmail.com>
2020-04-15 23:38:19 -0300

    fix: keep alive

--- server/python/src/whatsapp.py
+++ server/python/src/whatsapp.py
@@ -145,6 +145,12 @@ class WhatsAppWebClient:
             self.onCloseCallback["func"](self.onCloseCallback)
         eprint("WhatsApp backend Websocket closed.")
 
+    def keepAlive(self):
+        if self.activeWs is not None:
+            self.activeWs.send("?,,")
+            eprint("?,,")
+            Timer(20.0, self.keepAlive).start()
+
     def onMessage(self, ws, message):
         try:
             messageSplit = message.split(",", 1)
@@ -210,7 +216,6 @@ class WhatsAppWebClient:
                     if isinstance(jsonObj, list) and len(jsonObj) > 0:					# check if the result is an array
                         eprint(json.dumps(jsonObj))
                         if jsonObj[0] == "Conn":
-                            Timer(25, lambda: self.activeWs.send('?,,')).start() # Keepalive Request
                             self.connInfo["clientToken"] = jsonObj[1]["clientToken"]
                             self.connInfo["serverToken"] = jsonObj[1]["serverToken"]
                             self.connInfo["browserToken"] = jsonObj[1]["browserToken"]
@@ -267,6 +272,7 @@ class WhatsAppWebClient:
                                 "logged in as "
                                 + jsonObj[1]["pushname"]
                                 + " (" + jsonObj[1]["wid"] + ")")
+                            Timer(25, self.keepAlive).start() # Keepalive Request
                         elif jsonObj[0] == "Stream":
                             pass
                         elif jsonObj[0] == "Props":
